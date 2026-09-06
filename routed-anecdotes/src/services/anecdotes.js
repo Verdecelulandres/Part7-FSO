c21fea2dd476bebc004c5dec9a8850e8e7e667cc
Andres Laverde <andres.laverde@protonmail.com>
2026-09-05 19:09:01 -0700

    update: add node_env for testing scripts

--- routed-anecdotes/src/services/anecdotes.js
+++ routed-anecdotes/src/services/anecdotes.js
@@ -1,4 +1,4 @@
-const baseUrl = 'http://localhost:3001/anecdotes'
+const baseUrl = `http://localhost:${process.env.NODE_ENV === 'test' ? '3002' : '3001'}/anecdotes`
 
 const getAll = async () => {
   const response = await fetch(baseUrl)

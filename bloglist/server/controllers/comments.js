ce770400d1c4fa8bd575509a3bf3a9adfc196f22
Andres Laverde <andres.laverde@protonmail.com>
2026-09-13 18:46:13 -0700

    update: handle comments creation in server

--- bloglist/server/controllers/comments.js
+++ bloglist/server/controllers/comments.js
@@ -1,11 +1,20 @@
 const commentRouter = require("express").Router();
 const Comment = require("../models/comment");
-// const Blog = require("../models/blog");
+const Blog = require("../models/blog");
 
 commentRouter.get("/", async (request, response) => {
   const comments = await Comment.find({});
   response.json(comments);
 });
-// commentRouter.post();
+commentRouter.post("/", async (request, response) => {
+  const { content, blogid } = request.body;
+  const blog = await Blog.findById(blogid);
+  const newComment = { content, blog: blog._id };
+  const comment = new Comment(newComment);
+  const result = await comment.save();
+  blog.comments = blog.comments.concat(result._id);
+  await blog.save();
+  response.status(201).json(result);
+});
 
 module.exports = commentRouter;
